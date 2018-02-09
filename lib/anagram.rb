64df92df643531b43e2c54dfb67dbbbbe357aef3
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-02-09 09:22:39 -0800

    change first spec for logic with meaningful fail.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 class AnagramMaker
-  def initialize (phrase)
-    @phrase = phrase
+  def initialize (phrase1, phrase2)
+    @phrase1 = phrase1
+    @phrase2 = phrase2
   end
 
   def anagram_maker()
-
+    # letter_array = @phrase.split("")
+    # letter_array
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,7 +4,7 @@ require ('pry')
 
 describe ('#anagram_maker') do
   it ("will tell you if act is an anagram of cat") do
-    anagram = AnagramMaker.new("act")
-    expect(anagram.anagram_maker().to(eq("cat")))
+    anagram = AnagramMaker.new("act","cat")
+    expect(anagram.anagram_maker().to(eq(true)))
   end
 end
