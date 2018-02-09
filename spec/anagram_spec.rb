c9a6fcb8d458db313e60e5d9c7d9e85a542781bb
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-02-09 10:51:38 -0800

    add second spec looking for misspelling. Meaningful fail found.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -8,7 +8,11 @@ describe ('#anagram_maker') do
     expect(anagram.anagram_maker()).to(eq("Yes, 'act' is an anagram of 'cat'!"))
   end
   it ("will tell you if act is an anagram of Cat") do
-    anagram = AnagramMaker.new("act", "Cat")
-    expect(anagram.anagram_maker()).to(eq("Yes, 'act' is an anagram of 'Cat'!"))
+    anagram = AnagramMaker.new("act", "CaT")
+    expect(anagram.anagram_maker()).to(eq("Yes, 'act' is an anagram of 'CaT'!"))
+  end
+  it ("identify false words") do
+    anagram = AnagramMaker.new("cat", "actt")
+    expect(anagram.anagram_maker()).to(eq("Um, 'actt' isn't a word, dummy."))
   end
 end
